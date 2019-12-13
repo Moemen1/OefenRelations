46b982eac7a15363b0f26ca074f2461c7af1db1e
Moemen1 <moemenbadawi@hotmail.com>
2019-12-13 14:32:43 +0100

    Add project files.

---
+++ OefenRelations/Migrations/20191213091321_InitCreate.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OefenRelations.Migrations
+{
+    public partial class InitCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Country",
+                columns: table => new
+                {
+                    CountryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CountryNaam = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Country", x => x.CountryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Studenten",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    StudentNaam = table.Column<string>(nullable: true),
+                    CountryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Studenten", x => x.StudentId);
+                    table.ForeignKey(
+                        name: "FK_Studenten_Country_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Country",
+                        principalColumn: "CountryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Studenten_CountryId",
+                table: "Studenten",
+                column: "CountryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Studenten");
+
+            migrationBuilder.DropTable(
+                name: "Country");
+        }
+    }
+}
---
+++ OefenRelations/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OefenRelations
+{
+    class MyContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TestRelations;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        public DbSet<Student> Studenten { get; set; }
+    }
+    class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentNaam { get; set; }
+
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+    }
+
+    class Country
+    {
+        public int CountryId { get; set; }
+        public string CountryNaam { get; set; }
+
+        public List<Student> Studenten { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MyContext())
+            {
+                Student st = context.Studenten.First();
+                context.Entry(st).Reference(x => x.Country).Load();
+
+                foreach (Student s in context.Studenten.Include(s => s.Country)) 
+                {
+                    Console.WriteLine(String.Format("De student {0} met id {1} woont in {2}",
+                        s.StudentNaam, s.StudentId, s.Country.CountryNaam));
+                }            
+               
+            }
+        }
+    }
+}
